cbbad7271ea479b762087a44ff01d0000d3acb71
vishnupriya2801 <nxk164730@utdallas.edu>
2018-01-05 18:51:58 -0600

    Add files via upload

---
+++ att_d.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 14:25:16 2018
+
+@author: vishnupriyakodavatiganti
+"""
+# Import libraries
+import os,cv2
+import numpy as np
+#import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
+#%%
+os.chdir('/Users/vishnupriyakodavatiganti/Desktop')
+PATH = os.getcwd()
+# Define data path
+data_path = PATH + '/att'
+data_dir_list = os.listdir(data_path)
+
+img_rows=128
+img_cols=128
+num_channel=1
+num_epoch=25
+
+# Define the number of classes
+num_classes = 41
+
+img_data_list=[]
+
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		input_img_resize=cv2.resize(input_img,(128,128))
+		img_data_list.append(input_img_resize)
+
+img_data = np.array(img_data_list)
+img_data = img_data.astype('float32')
+img_data /= 255
+print (img_data.shape)
+
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		img_data= np.expand_dims(img_data, axis=1) 
+		print (img_data.shape)
+	else:
+		img_data= np.expand_dims(img_data, axis=4) 
+		print (img_data.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		img_data=np.rollaxis(img_data,3,1)
+		print (img_data.shape)
+		
+#%%
+USE_SKLEARN_PREPROCESSING=False
+
+if USE_SKLEARN_PREPROCESSING:
+	# using sklearn for preprocessing
+	from sklearn import preprocessing
+	
+	def image_to_feature_vector(image, size=(128, 128)):
+		# resize the image to a fixed size, then flatten the image into
+		# a list of raw pixel intensities
+		return cv2.resize(image, size).flatten()
+	
+	img_data_list=[]
+	for dataset in data_dir_list:
+		img_list=os.listdir(data_path+'/'+ dataset)
+		print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+		for img in img_list:
+			input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+			input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+			input_img_flatten=image_to_feature_vector(input_img,(128,128))
+			img_data_list.append(input_img_flatten)
+	
+	img_data = np.array(img_data_list)
+	img_data = img_data.astype('float32')
+	print (img_data.shape)
+	img_data_scaled = preprocessing.scale(img_data)
+	print (img_data_scaled.shape)
+	
+	print (np.mean(img_data_scaled))
+	print (np.std(img_data_scaled))
+	
+	print (img_data_scaled.mean(axis=0))
+	print (img_data_scaled.std(axis=0))
+	
+	if K.image_dim_ordering()=='th':
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
+		print (img_data_scaled.shape)
+		
+	else:
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
+		print (img_data_scaled.shape)
+	
+	
+	if K.image_dim_ordering()=='th':
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
+		print (img_data_scaled.shape)
+		
+	else:
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
+		print (img_data_scaled.shape)
+
+if USE_SKLEARN_PREPROCESSING:
+	img_data=img_data_scaled
+#%%
+# Assigning Labels
+
+# Define the number of classes
+num_classes = 41
+
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+j=0
+k=10
+for i in range(0,41):
+    labels[j:k]=i
+    j=j+10
+    k=k+10
+
+
+print(labels)
+ 
+for i in range(1,42):
+    names="s"
+    j=i
+    names=names+(str(j))
+    print(names)
+	  
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+#%%
+# Defining the model
+input_shape=img_data[0].shape
+					
+model = Sequential()
+
+model.add(Convolution2D(32, 3,3,border_mode='same',input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+#%%
+# Viewing model_configuration
+
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+
+#%%
+# Training
+hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test))
+
+#%%
+
+# Evaluating the model
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+test_image = X_test[0:1]
+print (test_image.shape)
+
+print(model.predict(test_image))
+print(model.predict_classes(test_image))
+print(y_test[0:1])
+#%%
+# Testing a new image
+test_image = cv2.imread('new/vishnu3.jpg')
+test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+test_image=cv2.resize(test_image,(128,128))
+test_image = np.array(test_image)
+test_image = test_image.astype('float32')
+test_image /= 255
+print (test_image.shape)
+   
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		test_image= np.expand_dims(test_image, axis=0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=3) 
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		test_image=np.rollaxis(test_image,2,0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+# Predicting the test image
+print((model.predict(test_image)))
+print(model.predict_classes(test_image))
+
+
+#%%
+# Printing the confusion matrix
+from sklearn.metrics import classification_report,confusion_matrix
+
+
+Y_pred = model.predict(X_test)
+print(Y_pred)
+y_pred = np.argmax(Y_pred, axis=1)
+print(y_pred)
+#y_pred = model.predict_classes(X_test)
+#print(y_pred)
+for i in range(1,42):
+    target_names="s"
+    j=i
+    target_names=target_names+(str(j))
+
+					
+print(classification_report(np.argmax(y_test,axis=1), y_pred,target_names=target_names))
+
+print(confusion_matrix(np.argmax(y_test,axis=1), y_pred))
